035037cc4bc8b2fdd60848eadf90fddb430a8017
chenzhiang05 <1981726707@qq.com>
2024-09-16 17:16:21 +0800

    Update Admissioncard.h

--- Admissioncard.h
+++ Admissioncard.h
@@ -9,9 +9,9 @@ class school;
 class Card                                        //准考证类
 {
 public:
-	Card( string Name="noname", string Admission="00000000", string Id="noid", char Gender='m');
-	void Set(string Name,string Admission,string Id,char Gender);
-	friend ostream & operator<<(ostream &out, const Card &c);       //输出重载函数
+	Card( string Name="noname", string Admission="00000000", string Id="noid", char Gender='m'); //构造函数
+	void Set(string Name,string Admission,string Id,char Gender);   //设置准考证信息
+	friend ostream & operator<<(ostream &out, const Card &c);       //重载输出流运算符函数
 	friend class School;
 
 private:
@@ -22,11 +22,11 @@ private:
 class School //学校类
 {
 public:
-	School(string Schoolname="noname");
-	void Set(string Schoolname);
-	operator string() const;
+	School(string Schoolname="noname");       //构造函数
+	void Set(string Schoolname);              //设置学校信息
+	operator string() const;                  //转换School对象类型为string
 	void Show(ostream &out) const;            //显示不同学校的链表
-	friend ostream & operator<<(ostream &out, const School &s);
+	friend ostream & operator<<(ostream &out, const School &s);   //重载输出流运算符函数
         void AppendCard(const Card &c);           //追加考生准考结点
         void Display(std::ostream &out) const;    //显示同一学校的不同考生准考证信息的链表
    
