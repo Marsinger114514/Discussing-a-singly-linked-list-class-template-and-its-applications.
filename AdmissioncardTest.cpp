7c0a2070b4e5ef31c2b94b62c59824829fdbd7df
chenzhiang05 <1981726707@qq.com>
2024-09-17 19:19:06 +0800

    Add files via upload
    陈

--- AdmissioncardTest.cpp
+++ AdmissioncardTest.cpp
@@ -1,62 +1,95 @@
 #include <conio.h>
 #include "Admissioncard.h"
 #include "linklist.h"
+#include <string>
 using namespace std;
+
 void AdmissionTest()
 {
     LinkList<School> uLink;
     School x_school;
     Node<School> *pU;
     Card x_card;
-    char schoolname[80],name[80],admission[80],id[80],str[80];
-    char gender;                  //学校名，姓名，准考证号，身份证号，性别
+    string schoolname, name, admission, id, str;
+    string gender;                  // 学校名，姓名，准考证号，身份证号，性别
     int choice;
-    while(true)
+    bool continueMainLoop = true;
+
+    while (continueMainLoop)
     {
         cout << "\n请输入学校(请输入汉字，直接回车则退出)：";
-        cin.getline(schoolname, 80);
-        if (schoolname[0] == '\0') {
-            cout << "退出录入程序。" << endl;
+        getline(cin, schoolname);
+
+        if (schoolname.empty()) { // 改成检查字符串是否为空
+            continueMainLoop = false; // 设置为 false 表示退出循环
             break;
         }
-        else{
+        else {
             x_school.Set(schoolname);
-            cout << "设置学校名称为: "<< schoolname << endl;
+            cout << "设置学校名称为: " << schoolname << endl;
             uLink.Append(x_school);
         }
 
         pU = uLink.Locate(string(x_school), true);
 
-        if(pU==nullptr)
+        if (pU == nullptr)
         {
-            cout << "学校姓名输入错误！" << endl;
+            cout << "学校名称输入错误！" << endl;
             continue;
         }
 
-        while(true)
+        bool continueSubLoop = true;
+
+        while (continueSubLoop)
         {
             cout << endl;
             uLink.CurData().Show(cout);
             cout << "1 -- 录入准考证数据" << endl;
-            cout << "2 -- 查询" << endl;
-            cout << "0-- 返回" << endl;
+            cout << "2 -- 查询该校考生信息" << endl;
+            cout << "3 -- 查询所有考生信息" << endl;
+            cout << "0 -- 返回" << endl;
             cout << "请选择：";
             choice = getche() - '0';
             cout << endl;
-            if(choice <= 0) break;
+
+            if (choice == 0)
+            {
+                continueSubLoop = false;
+                continue;
+            }
+
             switch (choice)
             {
-                case 1:
+            case 1:
+                {
                     cout << "请输入（学生姓名 学生性别 学生准考证号（8位） 学生身份证号（8位））: ";
                     cin >> name >> gender >> admission >> id;
-                    cin.getline(str, 80);
+                    cin.ignore(); 
                     x_card.Set(name, gender, admission, id);
                     uLink.CurData().AppendCard(x_card);
-                    break;
-                case 2:
-                    cout << uLink.CurData() << endl; break;
+                }
+                break;
+            case 2:
+                cout << uLink.CurData() << endl;
+                break;
+            case 3:
+                {
+                    cout << "所有考生信息：" << endl;
+                    for (pU = uLink.GoTop(); pU != nullptr; pU = uLink.Skip())
+                    {
+                        
+                        
+                       cout << uLink.CurData() << endl;
+                    }
+                }
+                break;
+            default:
+                cout << "无效的选择，请重新输入！" << endl;
+                break;
             }
         }
     }
-    wcout << "\n自动调用析构函数释放所有结点，返回主函数。\n" << endl;
+
+    cout << "\n自动调用析构函数释放所有结点，返回主函数。\n" << endl;
 }
+
